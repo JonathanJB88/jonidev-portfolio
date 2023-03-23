04de58774b1ad5d770d80411a05363d8bc152598
Jonathan Bracho <jonajes0288@gmail.com>
2023-03-23 15:26:11 +0100

    [update]: intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -15,7 +15,7 @@ export const Intro = ({ about }: IntroProps) => {
         <p className='max-w-screen-md mx-auto mt-4 text-xs md:text-base font-body dark:text-secondary'>{about}</p>
       </div>
       <div className='animate-slide-in'>
-        <Link href='#skills'>
+        <Link href='/skills'>
           <button className='px-4 py-2 mt-8 font-semibold transition-colors duration-300 border-2 rounded-md font-body text-accent border-accent hover:bg-accent-hover hover:text-primary'>
             Explore Skills
           </button>
